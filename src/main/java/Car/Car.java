984b41c1c94008f0c0fbd679d396bb6d9bd50a69
jiangyiqing <1181385834@qq.com>
2023-06-01 21:43:46 +0800

    Removed Car QueueSeq

--- src/main/java/Car/Car.java
+++ src/main/java/Car/Car.java
@@ -1,23 +1,20 @@
 package Car;
 
 public class Car implements Comparable<Car>{
-    private static int nextPrimaryKey = 0;
+    //private static int nextPrimaryKey = 0;
     private boolean isFastCharge; //是否是快充模式
-    private int QueueSeq; //到达的序号
     protected int PrimaryKey;
     private double RequestedChargingCapacity; //车辆的请求充电量
     private double CarBatteryCapacity;// 车辆的电池容量
-    public Car(boolean isFastCharge, int queueSeq, double requestedChargingCapacity, double carBatteryCapacity) {
+    public Car(boolean isFastCharge,  double requestedChargingCapacity, double carBatteryCapacity) {
         this.isFastCharge = isFastCharge;
-        QueueSeq = queueSeq;
-        PrimaryKey = nextPrimaryKey;
+        //PrimaryKey = nextPrimaryKey;
         RequestedChargingCapacity = requestedChargingCapacity;
         CarBatteryCapacity = carBatteryCapacity;
-        nextPrimaryKey ++;
+        //nextPrimaryKey ++;
     }
-    public Car(boolean isFastCharge, int queueSeq, double requestedChargingCapacity, double carBatteryCapacity, int primaryKey) {
+    public Car(boolean isFastCharge, double requestedChargingCapacity, double carBatteryCapacity, int primaryKey) {
         this.isFastCharge = isFastCharge;
-        QueueSeq = queueSeq;
         PrimaryKey = primaryKey;
         RequestedChargingCapacity = requestedChargingCapacity;
         CarBatteryCapacity = carBatteryCapacity;
@@ -43,13 +40,7 @@ public class Car implements Comparable<Car>{
         this.isFastCharge = isFastCharge;
     }
 
-    public int getQueueSeq() {
-        return QueueSeq;
-    }
 
-    public void setQueueSeq(int queueSeq) {
-        QueueSeq = queueSeq;
-    }
 
     public int getPrimaryKey() {
         return PrimaryKey;
@@ -73,19 +64,18 @@ public class Car implements Comparable<Car>{
             throw new NullPointerException("Don't Give Me a NULL Car.Car!");
         }
         if (o.getClass() == this.getClass()) {
-            return this.QueueSeq - o.QueueSeq;
+            return (int) (this.RequestedChargingCapacity - o.RequestedChargingCapacity);
         }
         throw new IllegalArgumentException("Don't Give Me something that is NOT a Car.Car!");
     }
     public Car getDeepCopy() {
-       return new Car(this.isFastCharge, this.getQueueSeq(), this.getRequestedChargingCapacity(), this.getCarBatteryCapacity(), this.PrimaryKey);
+       return new Car(this.isFastCharge, this.getRequestedChargingCapacity(), this.getCarBatteryCapacity(), this.PrimaryKey);
     }
 
     @Override
     public String toString() {
         return "Car{" +
                 "isFastCharge=" + isFastCharge +
-                ", QueueSeq=" + QueueSeq +
                 ", PrimaryKey=" + PrimaryKey +
                 ", RequestedChargingCapacity=" + RequestedChargingCapacity +
                 ", CarBatteryCapacity=" + CarBatteryCapacity +
