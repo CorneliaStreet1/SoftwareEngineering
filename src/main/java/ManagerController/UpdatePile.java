6476923b0b69c3f09b0680707862867523d37d9b
cottonb <472186159@qq.com>
2023-06-04 23:35:19 +0800

    管理员部分完成“

--- src/main/java/ManagerController/UpdatePile.java
+++ src/main/java/ManagerController/UpdatePile.java
@@ -1,5 +1,9 @@
 package ManagerController;
 
+import com.google.gson.Gson;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,11 +11,47 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UpdatePile extends HttpServlet {
+
+    enum Status{
+        RUNNING,
+        SHUTDOWN,
+        UNAVAILABLE;
+    }
+
+    static class ReqBody {
+        String pile_id;
+        Status status;
+    }
+
+    static class ResponseMsg {
+        int code;
+        String message;
+
+        ResponseMsg(int code, String message, QueryPile.RData[] data) {
+            this.code = code;
+            this.message = message;
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         {
             "code": 0,
                 "message": "success"
         }
+
+        resp.setContentType("application/json");
+
+        String token = req.getHeader("Authorization");
+
+        Claims claims = Jwts.parser()
+                .setSigningKey("secretKey")
+                .parseClaimsJws(token)
+                .getBody();
+
+        String userIdStr = claims.getSubject();
+        int userId = Integer.parseInt(userIdStr);
+
+        Gson gson = new Gson();
     }
 }
